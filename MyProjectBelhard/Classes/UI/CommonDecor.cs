f99580d605e533edf43504851690cb0f0105b8db
Eugene Vanifatev <eugenevanifatev@gmail.com>
2021-12-15 22:21:01 +0300

    Добавьте файлы проекта.

---
+++ MyProjectBelhard/Classes/UI/CommonDecor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProjectBelhard.Interfaces;
+
+namespace MyProjectBelhard.Classes.UI
+{
+    internal class CommonDecor : ICommonDecorable
+    {
+        public void GetHeader()
+        {
+            string centerText = "Shop";
+            int centerX = (Console.WindowWidth / 2) - (centerText.Length / 2);
+            Console.SetCursorPosition(centerX, 0);
+            Console.WriteLine(centerText);
+            for(int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.SetCursorPosition(i, 2);
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
---
+++ MyProjectBelhard/Classes/UI/StartPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProjectBelhard.Classes.Model;
+
+namespace MyProjectBelhard.Classes.UI
+{
+    internal class StartPage : CommonDecor
+    {
+        public StartPage()
+        {
+            GetHeader();
+            GetStartMenu();
+        }
+
+        void GetStartMenu()
+        {
+            Console.WriteLine("1. Log In");
+            LogIn logIn = new LogIn();
+
+            Console.WriteLine("2. Registration");
+        }
+    }
+}
